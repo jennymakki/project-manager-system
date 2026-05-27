6b1212ac5563d9bf08b77e1bb48337dee7273550
jennymakki <jjennyeklundd@hotmail.com>
2026-05-27 10:21:36 +0200

    fix: add missing /login route and fix protected route redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import BoardPage from "./pages/BoardPage";
-
-import ProtectedRoute from "./routes/ProtectedRoute";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        <Route
-          path="/boards/:id"
-          element={
-            <ProtectedRoute>
-              <BoardPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function ProtectedRoute() {
   const token = localStorage.getItem("token");
 
-  return token ? <Outlet /> : <Navigate to="/dashboard" />;
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
 }
